612aa05d1b1c56401036193c0ccd13e61cc9e2fb
Hsiaoming Yang <me@lepture.com>
2017-10-21 09:42:10 -0700

    Init project

---
+++ authlib/client/oauth1.py
@@ -0,0 +1,7 @@
+
+from requests import Session
+
+
+class OAuth1Session(Session):
+    def __init__(self, client_key, client_secret=None):
+        pass
---
+++ authlib/client/oauth2.py
@@ -0,0 +1,7 @@
+
+from requests import Session
+
+
+class OAuth2Session(Session):
+    def __init__(self, client_key, client_secret=None):
+        pass
