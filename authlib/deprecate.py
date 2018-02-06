3da8e392c16911f2736d55bb571a29d2a55747ca
Hsiaoming Yang <me@lepture.com>
2018-02-06 20:22:38 +0900

    Make deprecate time clear

--- authlib/deprecate.py
+++ authlib/deprecate.py
@@ -10,5 +10,5 @@ warnings.simplefilter('always', AuthlibDeprecationWarning)
 
 def deprecate(message, version=None, stacklevel=2):
     if version:
-        message += '\n\nIt will be compatible until version {}.'.format(version)
+        message += '\n\nIt will be compatible before version {}.'.format(version)
     warnings.warn(AuthlibDeprecationWarning(message), stacklevel=stacklevel)
