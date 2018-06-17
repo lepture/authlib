6be21ea5758e14a3ee612bcf264f49d5957a9e8a
Hsiaoming Yang <me@lepture.com>
2018-06-17 15:24:22 +0900

    Comments on RFC7636 module.

---
+++ authlib/specs/rfc7636/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    authlib.specs.rfc7636
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    This module represents a direct implementation of
+    Proof Key for Code Exchange by OAuth Public Clients.
+
+    https://tools.ietf.org/html/rfc7636
+"""
+
+from .grant import AuthorizationCodeGrant
