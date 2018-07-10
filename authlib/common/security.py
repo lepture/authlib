a8412da3fb917087722ab4b8dbd11b2223c67704
Max Murashov <m.murashov@rambler-co.ru>
2018-07-10 12:30:38 +0300

    Add handling of tokens without spaces

--- authlib/common/security.py
+++ authlib/common/security.py
@@ -7,7 +7,7 @@ UNICODE_ASCII_CHARACTER_SET = string.ascii_letters + string.digits
 
 def generate_token(length=30, chars=UNICODE_ASCII_CHARACTER_SET):
     rand = random.SystemRandom()
-    return ''.join(rand.choice(chars) for x in range(length))
+    return ''.join(rand.choice(chars) for _ in range(length))
 
 
 def is_secure_transport(uri):
