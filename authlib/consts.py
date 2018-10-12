04f4d7b58b08ede17703ad868066ab997bb48640
Hsiaoming Yang <me@lepture.com>
2018-10-12 11:24:23 +0900

    Prepare v0.11

--- authlib/consts.py
+++ authlib/consts.py
@@ -1,5 +1,5 @@
 name = 'Authlib'
-version = '0.10'
+version = '0.11.dev'
 author = 'Hsiaoming Yang <me@lepture.com>'
 homepage = 'https://authlib.org/'
 default_user_agent = '{}/{} (+{})'.format(name, version, homepage)
