aa28a07bf12d6151418a87808d76da8e165c6ae4
Hsiaoming Yang <me@lepture.com>
2019-03-19 15:35:39 +0800

    Fix #115 check_client_type for refresh_token.

--- authlib/oauth2/rfc6749/grants/refresh_token.py
+++ authlib/oauth2/rfc6749/grants/refresh_token.py
@@ -37,7 +37,7 @@ class RefreshTokenGrant(BaseGrant):
         client = self.authenticate_token_endpoint_client()
         log.debug('Validate token request of %r', client)
 
-        if client.check_client_type('public'):
+        if not client.check_client_type('confidential'):
             raise UnauthorizedClientError()
 
         if not client.check_grant_type(self.GRANT_TYPE):
