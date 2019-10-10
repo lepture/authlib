fc6fb3b46d57fa4da6e4f88ee497f175711a39f2
Hsiaoming Yang <me@lepture.com>
2019-10-10 20:52:24 +0800

    Add userinfo and parse_id_token in starlette client

--- authlib/integrations/starlette_client/remote_app.py
+++ authlib/integrations/starlette_client/remote_app.py
@@ -2,6 +2,8 @@ import inspect
 import logging
 
 from authlib.common.urls import urlparse
+from authlib.jose import JsonWebToken, jwk
+from authlib.oidc.core import UserInfo, CodeIDToken, ImplicitIDToken
 from starlette.responses import RedirectResponse
 from .._client import BaseApp
 from .._client import (
@@ -164,3 +166,71 @@ class RemoteApp(BaseApp):
 
             client.token = token
             return await client.request(method, url, **kwargs)
+
+    async def userinfo(self, **kwargs):
+        """Fetch user info from ``userinfo_endpoint``."""
+        metadata = await self._load_server_metadata()
+        resp = await self.get(metadata['userinfo_endpoint'], **kwargs)
+        data = resp.json()
+
+        compliance_fix = metadata.get('userinfo_compliance_fix')
+        if compliance_fix:
+            if inspect.iscoroutinefunction(compliance_fix):
+                data = await compliance_fix(self, data)
+            else:
+                data = compliance_fix(self, data)
+        return UserInfo(data)
+
+    async def parse_id_token(self, request, token, claims_options=None):
+        """Return an instance of UserInfo from token's ``id_token``."""
+        if 'id_token' not in token:
+            return None
+
+        nonce = self._get_session_data(request, 'nonce')
+        claims_params = dict(
+            nonce=nonce,
+            client_id=self.client_id,
+        )
+        if 'access_token' in token:
+            claims_params['access_token'] = token['access_token']
+            claims_cls = CodeIDToken
+        else:
+            claims_cls = ImplicitIDToken
+
+        metadata = await self._load_server_metadata()
+        if claims_options is None and 'issuer' in metadata:
+            claims_options = {'iss': {'values': [metadata['issuer']]}}
+
+        alg_values = metadata.get('id_token_signing_alg_values_supported')
+        if not alg_values:
+            alg_values = ['RS256']
+
+        jwk_set = await self._fetch_jwk_set()
+
+        def load_key(header, payload):
+            # TODO: reload jwk set if invalid
+            return jwk.loads(jwk_set, header.get('kid'))
+
+        jwt = JsonWebToken(alg_values)
+        claims = jwt.decode(
+            token['id_token'], key=load_key,
+            claims_cls=claims_cls,
+            claims_options=claims_options,
+            claims_params=claims_params,
+        )
+        claims.validate(leeway=120)
+        return UserInfo(claims)
+
+    async def _fetch_jwk_set(self, force=False):
+        metadata = await self._load_server_metadata()
+        jwk_set = metadata.get('jwks')
+        if jwk_set and not force:
+            return jwk_set
+
+        uri = metadata.get('jwks_uri')
+        if not uri:
+            raise RuntimeError('Missing "jwks_uri" in metadata')
+
+        jwk_set = await self._fetch_server_metadata(uri)
+        self.server_metadata['jwks'] = jwk_set
+        return jwk_set
