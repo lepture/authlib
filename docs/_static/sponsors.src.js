be11ee5ac6601644d94c78270de69bfcfda5220f
Hsiaoming Yang <me@lepture.com>
2020-03-20 16:26:03 +0900

    Update sponsors js

--- docs/_static/sponsors.src.js
+++ docs/_static/sponsors.src.js
@@ -7,20 +7,10 @@
     div.className = 'fund';
     var s = document.createElement('script');
     s.async = 1;
-    if (Math.random() > 0.6) {
-      div.id = 'codefund';
-      s.src = "https://app.codefund.io/properties/609/funder.js";
-      if (Math.random() > 0.5) {
-        s.src += '?template=horizontal';
-      }
-      h2.parentNode.insertBefore(div, h2);
-      document.head.appendChild(s);
-    } else {
-      s.id = '_carbonads_js';
-      s.src = 'https://cdn.carbonads.com/carbon.js?serve=CE7DKK3W&placement=authliborg';
-      div.appendChild(s);
-      h2.parentNode.insertBefore(div, h2);
-    }
+    s.id = '_carbonads_js';
+    s.src = 'https://cdn.carbonads.com/carbon.js?serve=CE7DKK3W&placement=authliborg';
+    div.appendChild(s);
+    h2.parentNode.insertBefore(div, h2);
   }
 
   document.addEventListener('DOMContentLoaded', pageAd);
