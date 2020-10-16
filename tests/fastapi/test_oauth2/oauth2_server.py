3e30635cde4637b9107c2d25d407e42561018f39
Gabriel Machado Santos <gmachado@nextreason.com>
2020-10-16 19:04:09 -0300

    Implemented the authorization code grant pytests

---
+++ tests/fastapi/test_oauth2/oauth2_server.py
@@ -0,0 +1,120 @@
+import os
+import base64
+import unittest
+from fastapi import FastAPI, Request, Form
+from fastapi.testclient import TestClient
+from authlib.common.security import generate_token
+from authlib.common.encoding import to_bytes, to_unicode
+from authlib.common.urls import url_encode
+from authlib.integrations.sqla_oauth2 import (
+    create_query_client_func,
+    create_save_token_func,
+)
+from authlib.integrations.fastapi_oauth2 import AuthorizationServer
+from authlib.oauth2 import OAuth2Error
+from .models import User, Client, Token
+from .database import Base, engine, db
+
+os.environ['AUTHLIB_INSECURE_TRANSPORT'] = 'true'
+
+
+def token_generator(client, grant_type, user=None, scope=None):
+    token = '{}-{}'.format(client.client_id[0], grant_type)
+    if user:
+        token = '{}.{}'.format(token, user.get_user_id())
+    return '{}.{}'.format(token, generate_token(32))
+
+
+def create_authorization_server(app):
+    query_client = create_query_client_func(db, Client)
+    save_token = create_save_token_func(db, Token)
+
+    server = AuthorizationServer()
+    server.init_app(app, query_client, save_token)
+
+    @app.get('/oauth/authorize')
+    def authorize(request: Request):
+        user_id = request.query_params.get('user_id')
+        request.body = {}
+        if user_id:
+            end_user = db.query(User).filter(User.id == int(user_id)).first()
+        else:
+            end_user = None
+        try:
+            grant = server.validate_consent_request(request=request, end_user=end_user)
+            return grant.prompt or 'ok'
+        except OAuth2Error as error:
+            return url_encode(error.get_body())
+
+    @app.post('/oauth/authorize')
+    def authorize(request: Request, user_id: str = Form('')):
+        request.body = {}
+        if user_id:
+            grant_user = db.query(User).filter(User.id == int(user_id)).first()
+        else:
+            grant_user = None
+        return server.create_authorization_response(request=request, grant_user=grant_user)
+
+    @app.post('/oauth/token')
+    def issue_token(
+            request: Request,
+            grant_type: str = Form(...),
+            scope: str = Form(None),
+            code: str = Form(None),
+            refresh_token: str = Form(None),
+            code_verifier: str = Form(None),
+            client_id: str = Form(None),
+            client_secret: str = Form(None)):
+        request.body = {
+            'grant_type': grant_type,
+            'scope': scope,
+        }
+        if grant_type == 'authorization_code':
+            request.body['code'] = code
+        elif grant_type == 'refresh_token':
+            request.body['refresh_token'] = refresh_token
+
+        if code_verifier:
+            request.body['code_verifier'] = code_verifier
+
+        if client_id:
+            request.body['client_id'] = client_id
+
+        if client_secret:
+            request.body['client_secret'] = client_secret
+
+        return server.create_token_response(request=request)
+
+    return server
+
+
+def create_fastapi_app():
+    app = FastAPI()
+    app.debug = True
+    app.testing = True
+    app.secret_key = 'testing'
+    app.test_client = TestClient(app)
+    app.config = {
+        'OAUTH2_ERROR_URIS': [
+            ('invalid_client', 'https://a.b/e#invalid_client')
+        ]
+    }
+    return app
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app = create_fastapi_app()
+
+        Base.metadata.create_all(bind=engine)
+
+        self.app = app
+        self.client = app.test_client
+
+    def tearDown(self):
+        Base.metadata.drop_all(bind=engine)
+
+    def create_basic_header(self, username, password):
+        text = '{}:{}'.format(username, password)
+        auth = to_unicode(base64.b64encode(to_bytes(text)))
+        return {'Authorization': 'Basic ' + auth}
---
+++ tests/fastapi/test_oauth2/test_authorization_code_grant.py
@@ -0,0 +1,254 @@
+import json
+from authlib.common.urls import urlparse, url_decode
+from authlib.oauth2.rfc6749.grants import (
+    AuthorizationCodeGrant as _AuthorizationCodeGrant,
+)
+from .database import db
+from .models import User, Client, AuthorizationCode
+from .models import CodeGrantMixin, save_authorization_code
+from .oauth2_server import TestCase
+from .oauth2_server import create_authorization_server
+
+
+class AuthorizationCodeGrant(CodeGrantMixin, _AuthorizationCodeGrant):
+    TOKEN_ENDPOINT_AUTH_METHODS = ['client_secret_basic', 'client_secret_post', 'none']
+
+    def save_authorization_code(self, code, request):
+        return save_authorization_code(code, request)
+
+
+class AuthorizationCodeTest(TestCase):
+
+    def register_grant(self, server):
+        server.register_grant(AuthorizationCodeGrant)
+
+    def prepare_data(
+            self, is_confidential=True,
+            response_type='code', grant_type='authorization_code',
+            token_endpoint_auth_method='client_secret_basic'):
+        server = create_authorization_server(self.app)
+        self.register_grant(server)
+        self.server = server
+
+        user = User(username='foo')
+        db.add(user)
+        db.commit()
+
+        if is_confidential:
+            client_secret = 'code-secret'
+        else:
+            client_secret = ''
+        client = Client(
+            user_id=user.id,
+            client_id='code-client',
+            client_secret=client_secret,
+        )
+        client.set_client_metadata({
+            'redirect_uris': ['https://a.b'],
+            'scope': 'profile address',
+            'token_endpoint_auth_method': token_endpoint_auth_method,
+            'response_types': [response_type],
+            'grant_types': grant_type.splitlines(),
+        })
+        self.authorize_url = (
+            '/oauth/authorize?response_type=code'
+            '&client_id=code-client'
+        )
+        db.add(client)
+        db.commit()
+
+    def test_get_authorize(self):
+        self.prepare_data()
+        rv = self.client.get(self.authorize_url)
+        self.assertEqual(rv.json(), 'ok')
+
+    def test_invalid_client_id(self):
+        self.prepare_data()
+        url = '/oauth/authorize?response_type=code'
+        rv = self.client.get(url)
+        self.assertIn('invalid_client', rv.json())
+
+        url = '/oauth/authorize?response_type=code&client_id=invalid'
+        rv = self.client.get(url)
+        self.assertIn('invalid_client', rv.json())
+
+    def test_invalid_authorize(self):
+        self.prepare_data()
+        rv = self.client.post(self.authorize_url)
+        self.assertIn('error=access_denied', rv.headers['location'])
+
+        self.server.metadata = {'scopes_supported': ['profile']}
+        rv = self.client.post(self.authorize_url + '&scope=invalid&state=foo')
+        self.assertIn('error=invalid_scope', rv.headers['location'])
+        self.assertIn('state=foo', rv.headers['location'])
+
+    def test_unauthorized_client(self):
+        self.prepare_data(True, 'token')
+        rv = self.client.get(self.authorize_url)
+        self.assertIn('unauthorized_client', rv.json())
+
+    def test_invalid_client(self):
+        self.prepare_data()
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'code': 'invalid',
+            'client_id': 'invalid-id',
+        })
+        resp = rv.json()
+        self.assertEqual(resp['error'], 'invalid_client')
+
+        headers = self.create_basic_header('code-client', 'invalid-secret')
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'code': 'invalid',
+        }, headers=headers)
+        resp = rv.json()
+        self.assertEqual(resp['error'], 'invalid_client')
+        self.assertEqual(resp['error_uri'], 'https://a.b/e#invalid_client')
+
+    def test_invalid_code(self):
+        self.prepare_data()
+
+        headers = self.create_basic_header('code-client', 'code-secret')
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+        }, headers=headers)
+        resp = rv.json()
+        self.assertEqual(resp['error'], 'invalid_request')
+
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'code': 'invalid',
+        }, headers=headers)
+        resp = rv.json()
+        self.assertEqual(resp['error'], 'invalid_request')
+
+        code = AuthorizationCode(
+            code='no-user',
+            client_id='code-client',
+            user_id=0
+        )
+        db.add(code)
+        db.commit()
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'code': 'no-user',
+        }, headers=headers)
+        resp = rv.json()
+        self.assertEqual(resp['error'], 'invalid_request')
+
+    def test_invalid_redirect_uri(self):
+        self.prepare_data()
+        uri = self.authorize_url + '&redirect_uri=https%3A%2F%2Fa.c'
+        rv = self.client.post(uri, data={'user_id': '1'})
+        resp = rv.json()
+        self.assertEqual(resp['error'], 'invalid_request')
+
+        uri = self.authorize_url + '&redirect_uri=https%3A%2F%2Fa.b'
+        rv = self.client.post(uri, data={'user_id': '1'})
+        self.assertIn('code=', rv.headers['location'])
+
+        params = dict(url_decode(urlparse.urlparse(rv.headers['location']).query))
+        code = params['code']
+        headers = self.create_basic_header('code-client', 'code-secret')
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'code': code,
+        }, headers=headers)
+        resp = rv.json()
+        self.assertEqual(resp['error'], 'invalid_request')
+
+    def test_invalid_grant_type(self):
+        self.prepare_data(
+            False, token_endpoint_auth_method='none',
+            grant_type='invalid'
+        )
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'client_id': 'code-client',
+            'code': 'a',
+        })
+        resp = rv.json()
+        self.assertEqual(resp['error'], 'unauthorized_client')
+
+    def test_authorize_token_no_refresh_token(self):
+        self.app.config.update({'OAUTH2_REFRESH_TOKEN_GENERATOR': True})
+        self.prepare_data(False, token_endpoint_auth_method='none')
+
+        rv = self.client.post(self.authorize_url, data={'user_id': '1'})
+        self.assertIn('code=', rv.headers['location'])
+
+        params = dict(url_decode(urlparse.urlparse(rv.headers['location']).query))
+        code = params['code']
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'code': code,
+            'client_id': 'code-client',
+        })
+        resp = rv.json()
+        self.assertIn('access_token', resp)
+        self.assertNotIn('refresh_token', resp)
+
+    def test_authorize_token_has_refresh_token(self):
+        # generate refresh token
+        self.app.config.update({'OAUTH2_REFRESH_TOKEN_GENERATOR': True})
+        self.prepare_data(grant_type='authorization_code\nrefresh_token')
+        url = self.authorize_url + '&state=bar'
+        rv = self.client.post(url, data={'user_id': '1'})
+        self.assertIn('code=', rv.headers['location'])
+
+        params = dict(url_decode(urlparse.urlparse(rv.headers['location']).query))
+        self.assertEqual(params['state'], 'bar')
+
+        code = params['code']
+        headers = self.create_basic_header('code-client', 'code-secret')
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'code': code,
+        }, headers=headers)
+        resp = rv.json()
+        self.assertIn('access_token', resp)
+        self.assertIn('refresh_token', resp)
+
+    def test_client_secret_post(self):
+        self.app.config.update({'OAUTH2_REFRESH_TOKEN_GENERATOR': True})
+        self.prepare_data(
+            grant_type='authorization_code\nrefresh_token',
+            token_endpoint_auth_method='client_secret_post',
+        )
+        url = self.authorize_url + '&state=bar'
+        rv = self.client.post(url, data={'user_id': '1'})
+        self.assertIn('code=', rv.headers['location'])
+
+        params = dict(url_decode(urlparse.urlparse(rv.headers['location']).query))
+        self.assertEqual(params['state'], 'bar')
+
+        code = params['code']
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'client_id': 'code-client',
+            'client_secret': 'code-secret',
+            'code': code,
+        })
+        resp = rv.json()
+        self.assertIn('access_token', resp)
+        self.assertIn('refresh_token', resp)
+
+    def test_token_generator(self):
+        m = 'tests.fastapi.test_oauth2.oauth2_server:token_generator'
+        self.app.config.update({'OAUTH2_ACCESS_TOKEN_GENERATOR': m})
+        self.prepare_data(False, token_endpoint_auth_method='none')
+
+        rv = self.client.post(self.authorize_url, data={'user_id': '1'})
+        self.assertIn('code=', rv.headers['location'])
+
+        params = dict(url_decode(urlparse.urlparse(rv.headers['location']).query))
+        code = params['code']
+        rv = self.client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'code': code,
+            'client_id': 'code-client',
+        })
+        resp = rv.json()
+        self.assertIn('access_token', resp)
+        self.assertIn('c-authorization_code.1.', resp['access_token'])
