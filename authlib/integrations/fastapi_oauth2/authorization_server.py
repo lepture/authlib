b0c8759dbdfbc972bc4cbf1d822d4d6fdadc6f7b
alexey.turlapov <aturlapov@janrain.com>
2020-12-14 17:56:57 -0600

    test commit

--- authlib/integrations/fastapi_oauth2/authorization_server.py
+++ authlib/integrations/fastapi_oauth2/authorization_server.py
@@ -1,16 +1,14 @@
 """Implementation of authlib.oauth2.rfc6749.AuthorizationServer class for FastAPI."""
 
 import json
-from werkzeug.utils import import_string
-from fastapi.responses import JSONResponse
-from authlib.oauth2 import (
-    OAuth2Request,
-    HttpRequest,
-    AuthorizationServer as _AuthorizationServer,
-)
+
+from authlib.common.security import generate_token
+from authlib.oauth2 import AuthorizationServer as _AuthorizationServer
+from authlib.oauth2 import HttpRequest, OAuth2Request
 from authlib.oauth2.rfc6750 import BearerToken
 from authlib.oauth2.rfc8414 import AuthorizationServerMetadata
-from authlib.common.security import generate_token
+from fastapi.responses import JSONResponse
+from werkzeug.utils import import_string
 
 
 class AuthorizationServer(_AuthorizationServer):
@@ -31,26 +29,30 @@ class AuthorizationServer(_AuthorizationServer):
 
         metadata_class = AuthorizationServerMetadata
 
-        metadata_file = app.config.get('OAUTH2_METADATA_FILE')
+        metadata_file = app.config.get("OAUTH2_METADATA_FILE")
         if metadata_file:
             with open(metadata_file) as metadata_file_content:
                 metadata = metadata_class(json.loads(metadata_file_content))
                 metadata.validate()
                 self.metadata = metadata
 
-        self.config.setdefault('error_uris', app.config.get('OAUTH2_ERROR_URIS'))
+        self.config.setdefault("error_uris", app.config.get("OAUTH2_ERROR_URIS"))
 
     def get_error_uris(self, request):
-        error_uris = self.config.get('error_uris')
+        error_uris = self.config.get("error_uris")
         if error_uris:
             return dict(error_uris)
         return None
 
     def create_oauth2_request(self, request):
-        return OAuth2Request(request.method, str(request.url), request.body, request.headers)
+        return OAuth2Request(
+            request.method, str(request.url), request.body, request.headers
+        )
 
     def create_json_request(self, request):
-        return HttpRequest(request.method, str(request.url), request.body, request.headers)
+        return HttpRequest(
+            request.method, str(request.url), request.body, request.headers
+        )
 
     def handle_response(self, status, body, headers):
         return JSONResponse(content=body, status_code=status, headers=dict(headers))
@@ -63,7 +65,7 @@ class AuthorizationServer(_AuthorizationServer):
 
         grant = self.get_authorization_grant(req)
         grant.validate_consent_request()
-        if not hasattr(grant, 'prompt'):
+        if not hasattr(grant, "prompt"):
             grant.prompt = None
         return grant
 
@@ -75,18 +77,16 @@ def create_bearer_token_generator(config):
     generate ``refresh_token``, which can be turn on by configuration
     ``OAUTH2_REFRESH_TOKEN_GENERATOR=True``.
     """
-    conf = config.get('OAUTH2_ACCESS_TOKEN_GENERATOR', True)
+    conf = config.get("OAUTH2_ACCESS_TOKEN_GENERATOR", True)
     access_token_generator = create_token_generator(conf, 42)
 
-    conf = config.get('OAUTH2_REFRESH_TOKEN_GENERATOR', False)
+    conf = config.get("OAUTH2_REFRESH_TOKEN_GENERATOR", False)
     refresh_token_generator = create_token_generator(conf, 48)
 
     expires_generator = create_token_expires_in_generator(config)
 
     return BearerToken(
-        access_token_generator,
-        refresh_token_generator,
-        expires_generator
+        access_token_generator, refresh_token_generator, expires_generator
     )
 
 
@@ -104,7 +104,7 @@ def create_token_expires_in_generator(config):
     data = {}
     data.update(BearerToken.GRANT_TYPES_EXPIRES_IN)
 
-    expires_in_conf = config.get('OAUTH2_TOKEN_EXPIRES_IN')
+    expires_in_conf = config.get("OAUTH2_TOKEN_EXPIRES_IN")
     if expires_in_conf:
         data.update(expires_in_conf)
 
@@ -123,8 +123,10 @@ def create_token_generator(token_generator_conf, length=42):
         return import_string(token_generator_conf)
 
     if token_generator_conf is True:
+
         def token_generator(*args, **kwargs):  # pylint: disable=W0613
             return generate_token(length)
+
         return token_generator
 
     return None
