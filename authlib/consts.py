d8e428c9350c792fc3d25dbaaffa3bfefaabd8e3
Hsiaoming Yang <me@lepture.com>
2021-10-18 21:08:43 +0900

    Version bump 0.15.5

--- authlib/consts.py
+++ authlib/consts.py
@@ -1,5 +1,5 @@
 name = 'Authlib'
-version = '0.15.4'
+version = '0.15.5'
 author = 'Hsiaoming Yang <me@lepture.com>'
 homepage = 'https://authlib.org/'
 default_user_agent = '{}/{} (+{})'.format(name, version, homepage)
