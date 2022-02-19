1680a493b907ecf81499ba73c655ba7dee04e736
Jonathon Fletcher <jonathon.fletcher@gmail.com>
2022-02-18 20:46:25 -0800

    skeleton quart client integration

---
+++ authlib/integrations/quart_client/remote_app.py
@@ -0,0 +1,81 @@
+from quart import redirect
+from quart import request as quart_req
+from quart import _app_ctx_stack
+from ..base_client import RemoteApp
+
+
+class QuartRemoteApp(RemoteApp):
+    """Quart integrated RemoteApp of :class:`~authlib.client.OAuthClient`.
+    It has built-in hooks for OAuthClient. The only required configuration
+    is token model.
+    """
+
+    def __init__(self, framework, name=None, fetch_token=None, **kwargs):
+        fetch_request_token = kwargs.pop('fetch_request_token', None)
+        save_request_token = kwargs.pop('save_request_token', None)
+        super(QuartRemoteApp, self).__init__(framework, name, fetch_token, **kwargs)
+
+        self._fetch_request_token = fetch_request_token
+        self._save_request_token = save_request_token
+
+    def _on_update_token(self, token, refresh_token=None, access_token=None):
+        self.token = token
+        super(QuartRemoteApp, self)._on_update_token(
+            token, refresh_token, access_token
+        )
+
+    @property
+    def token(self):
+        ctx = _app_ctx_stack.top
+        attr = 'authlib_oauth_token_{}'.format(self.name)
+        token = getattr(ctx, attr, None)
+        if token:
+            return token
+        if self._fetch_token:
+            token = self._fetch_token()
+            self.token = token
+            return token
+
+    @token.setter
+    def token(self, token):
+        ctx = _app_ctx_stack.top
+        attr = 'authlib_oauth_token_{}'.format(self.name)
+        setattr(ctx, attr, token)
+
+    async def request(self, method, url, token=None, **kwargs):
+        if token is None and not kwargs.get('withhold_token'):
+            token = self.token
+        return super(QuartRemoteApp, self).request(
+            method, url, token=token, **kwargs)
+
+    async def authorize_redirect(self, redirect_uri=None, **kwargs):
+        """Create a HTTP Redirect for Authorization Endpoint.
+
+        :param redirect_uri: Callback or redirect URI for authorization.
+        :param kwargs: Extra parameters to include.
+        :return: A HTTP redirect response.
+        """
+        rv = self.create_authorization_url(redirect_uri, **kwargs)
+
+        if self.request_token_url:
+            request_token = rv.pop('request_token', None)
+            self._save_request_token(request_token)
+
+        self.save_authorize_data(quart_req, redirect_uri=redirect_uri, **rv)
+        return redirect(rv['url'])
+
+    async def authorize_access_token(self, **kwargs):
+        """Authorize access token."""
+        if self.request_token_url:
+            request_token = self._fetch_request_token()
+        else:
+            request_token = None
+
+        params = self.retrieve_access_token_params(quart_req, request_token)
+        params.update(kwargs)
+        token = self.fetch_access_token(**params)
+        self.token = token
+        return token
+
+    def parse_id_token(self, token, claims_options=None, leeway=120):
+        return self._parse_id_token(quart_req, token, claims_options, leeway)
---
+++ authlib/integrations/quart_helpers.py
@@ -0,0 +1,25 @@
+from quart import request as quart_req
+from authlib.common.encoding import to_unicode
+
+
+async def create_oauth_request(request, request_cls, use_json=False):
+    if isinstance(request, request_cls):
+        return request
+
+    if not request:
+        request = quart_req
+
+    if request.method == 'POST':
+        if use_json:
+            body = await request.get_json()
+        else:
+            body = await request.form.to_dict(flat=True)
+    else:
+        body = None
+
+    # query string in werkzeug Request.url is very weird
+    # scope=profile%20email will be scope=profile email
+    url = request.base_url
+    if request.query_string:
+        url = url + '?' + to_unicode(request.query_string)
+    return await request_cls(request.method, url, body, request.headers)
