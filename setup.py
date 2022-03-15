51c1b7246e612ec5110c9f54bf2282714a8f9dbc
Hsiaoming Yang <me@lepture.com>
2022-03-15 23:27:13 +0900

    Fix for GitHub dependency graph
    https://github.com/lepture/authlib/issues/436

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+
+setup(
+    name="Authlib",
+    install_requires=[
+        "cryptography>=3.2",
+    ],
+)
