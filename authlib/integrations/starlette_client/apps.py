aca5caa1eccc877226923fde35a52041e34bc31b
Konstantin Köhring <konstantin.koehring@peerox.de>
2022-04-04 15:45:03 +0200

    Allow passing claims_options in DjangoOAuth2App, FlaskOAuth2App and StarletteOAuth2App

--- authlib/integrations/starlette_client/apps.py
+++ authlib/integrations/starlette_client/apps.py
@@ -69,12 +69,13 @@ class StarletteOAuth2App(StarletteAppMixin, AsyncOAuth2Mixin, AsyncOpenIDMixin, 
         else:
             session = request.session
 
+        claims_options = kwargs.pop('claims_options', None)
         state_data = await self.framework.get_state_data(session, params.get('state'))
         await self.framework.clear_state_data(session, params.get('state'))
         params = self._format_state_params(state_data, params)
         token = await self.fetch_access_token(**params, **kwargs)
 
         if 'id_token' in token and 'nonce' in state_data:
-            userinfo = await self.parse_id_token(token, nonce=state_data['nonce'])
+            userinfo = await self.parse_id_token(token, nonce=state_data['nonce'], claims_options=claims_options)
             token['userinfo'] = userinfo
         return token
