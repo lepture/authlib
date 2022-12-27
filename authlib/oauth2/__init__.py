a2ada05dae625695f559140675a8d2aebc6b5974
Hsiaoming Yang <me@lepture.com>
2022-12-27 16:21:20 +0900

    Fix importing JsonRequest

--- authlib/oauth2/__init__.py
+++ authlib/oauth2/__init__.py
@@ -3,7 +3,7 @@ from .auth import ClientAuth, TokenAuth
 from .client import OAuth2Client
 from .rfc6749 import (
     OAuth2Request,
-    HttpRequest,
+    JsonRequest,
     AuthorizationServer,
     ClientAuthentication,
     ResourceProtector,
@@ -11,6 +11,6 @@ from .rfc6749 import (
 
 __all__ = [
     'OAuth2Error', 'ClientAuth', 'TokenAuth', 'OAuth2Client',
-    'OAuth2Request', 'HttpRequest', 'AuthorizationServer',
+    'OAuth2Request', 'JsonRequest', 'AuthorizationServer',
     'ClientAuthentication', 'ResourceProtector',
 ]
