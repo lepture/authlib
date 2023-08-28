24bb40ec5bedf13b602be1eb5f3ffa1da037c1bf
Éloi Rivard <eloi@yaal.coop>
2023-08-28 09:41:24 +0200

    tests: django tests use in-memory sqlite database

--- tests/django/settings.py
+++ tests/django/settings.py
@@ -3,7 +3,7 @@ SECRET_KEY = 'django-secret'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "example.sqlite",
+        "NAME": ":memory:",
     }
 }
 
